f530dd17bc43b62024c87750be25a356747a97a9
Dmytro Klishch <klishch.dmytro@gmail.com>
2023-09-25 09:56:34 +0300

    Initial commit

---
+++ src/main/java/org/example/a/BearThread.java
@@ -0,0 +1,19 @@
+package org.example.a;
+
+public class BearThread extends Thread {
+
+    private final Bucket bucket;
+
+    public BearThread(Bucket bucket) {
+        this.bucket = bucket;
+    }
+
+    @Override
+    public void run() {
+        while (isAlive()) {
+            if (bucket.takeNotification()) {
+                bucket.eatAllHoney();
+            }
+        }
+    }
+}
