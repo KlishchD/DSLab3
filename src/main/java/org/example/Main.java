a622c0f05c622c0c54d41da2210d6a24f1ce8b0d
Dmytro Klishch <klishch.dmytro@gmail.com>
2023-09-26 10:46:55 +0300

    Added b

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.a.BeeApp;
+import org.example.b.HairSalonApp;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new HairSalonApp(2);
     }
 }
